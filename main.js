436120c9513a8f873e10a9d075c8566e6d9449e8
Vinicio <vinicioricci@gmail.com>
2022-10-26 18:16:04 -0600

    colores de los numeros arreglado

--- main.js
+++ main.js
@@ -5,18 +5,35 @@ const restar = document.getElementById("dec");
 const reiniciar = document.getElementById("rest");
 
 let num = 0;
+if(num == 0 ){
+    contador.style.color = "white";
+}
 
 sumar.addEventListener("click", ()=>{
     num++;
     contador.innerHTML = num;
+    if(num > 0 ){
+        contador.style.color = "green";
+    }else if(num == 0){
+        contador.style.color = "white";
+    }
 })
 
 restar.addEventListener("click", ()=>{
     num--;
     contador.innerHTML = num;
+    if(num < 0 ){
+        contador.style.color = "red";
+    }else if(num == 0){
+        contador.style.color = "white";
+    }
 })
 
 reiniciar.addEventListener("click", ()=>{
     num = 0;
     contador.innerHTML = num;
+    if(num == 0 ){
+        contador.style.color = "white";
+    }
 })
+
